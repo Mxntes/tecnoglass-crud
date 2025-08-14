23e45151c8b2cf569fcd4c8c69cc8bffe9a3c67a
Mxntes <miguel2jesus1@gmail.com>
2025-08-14 13:06:48 -0500

    ajuste headers en login

--- src/api/login-clean.js
+++ src/api/login-clean.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 
-export const api = axios.create({
+export const loginApi = axios.create({
   baseURL: import.meta.env.VITE_LOGIN_API_BASE || "https://reqres.in",
   timeout: 10000,
 });
 
 export async function loginReqres(email, password) {
-  const { data } = await api.post("/api/login", { email, password });
+  const { data } = await loginApi.post(
+    "/api/login",
+    { email, password },
+    {
+      headers: {
+        "x-api-key": import.meta.env.VITE_API_KEY,
+      },
+    }
+  );
   return data;
 }
